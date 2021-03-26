592c0fc96c46928dbecf83bcfee5116e7a335507
matsuzaki <fivestar0737@gmail.com>
2021-03-26 09:55:56 +0900

    add gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marvo/urdf_ws/src
-# Build directory: /home/marvo/urdf_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("robot_description")
--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/marvo/urdf_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/marvo/urdf_ws/devel;/opt/ros/melodic'.split(';') if '/home/marvo/urdf_ws/devel;/opt/ros/melodic' != '' else []
